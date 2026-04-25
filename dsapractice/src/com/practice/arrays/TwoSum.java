d2ccca904a0d694a8e15ad7671d92399c5c7b1ce
GordeYash <gordeyash798@gmail.com>
2026-04-25 07:34:19 +0530

    Problem SOlve

--- dsapractice/src/com/practice/arrays/TwoSum.java
+++ dsapractice/src/com/practice/arrays/TwoSum.java
@@ -1,10 +1,26 @@
 package com.practice.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
 
         // TODO: implement logic
+    	Map<Integer,Integer> data=new HashMap<Integer, Integer>();
+    	for(int i=0;i<nums.length;i++)
+    	{
+    		int complement=target-nums[i];
+    		//System.out.println("Complement :"+complement);
+    		if(data.containsKey(complement))
+    		{
+    			//System.out.println("Value in Map :"+data.containsKey(complement));
+    			return new int [] {data.get(complement),i};
+    		}
+    		//System.out.println("Puting data in map :"+complement+" : "+i);
+    		data.put(nums[i], i);
+    	}
 
         return new int[] {};
     }
@@ -13,8 +29,8 @@ public class TwoSum {
 
         TwoSum obj = new TwoSum();
 
-        int[] nums = {};
-        int target = 0;
+        int[] nums = {1,8,7,4,0};
+        int target = 5;
 
         int[] result = obj.twoSum(nums, target);
 
